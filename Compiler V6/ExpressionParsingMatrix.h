577fca6cc5273e005cb5982d00be3963d547db77
brenthompson2 <brenthompson2@gmail.com>
2017-11-08 23:55:27 -0500

    Defined the Matrix

--- Compiler V6/ExpressionParsingMatrix.h
+++ Compiler V6/ExpressionParsingMatrix.h
@@ -32,8 +32,29 @@ using std::string;
 	Symbolic Constants
 ============================================================================== */
 
-#define MAX_STWO 20
-#define MAX_INPUT_STRINGS 20
+#define NUM_INDEX_VALUES 13
+
+#define INDEX_NULL 0
+#define INDEX_ID 1
+#define INDEX_EQUALS 2
+#define INDEX_PLUS 3
+#define INDEX_MINUS 4
+#define INDEX_TIMES 5
+#define INDEX_DIVIDE 6
+#define INDEX_LEFT_PARENTHESIS 7
+#define INDEX_RIGHT_PARENTHESIS 8
+#define INDEX_EXPONENT 9
+#define INDEX_LEFT_BRACKET 10
+#define INDEX_RIGHT_BRACKET 11
+#define INDEX_EOL 12
+
+#define TOKEN_S_ONE 0
+#define TOKEN_S_TWO 1
+#define TOKEN_U_ONE 2
+#define TOKEN_U_TWO 3
+#define TOKEN_U_THREE 4
+#define TOKEN_U_FOUR 5
+#define TOKEN_ERROR 6
 
 /* ==============================================================================
 	Type Definitions
@@ -47,7 +68,7 @@ struct STwoOject {
 struct InputObject {
 	bool isID;
 	char operatorSymbol;
-	STwoOject topSTWOArray[MAX_STWO];
+	STwoOject topSTWOArray[NUM_INDEX_VALUES];
 };
 
 
@@ -56,7 +77,22 @@ struct InputObject {
 ============================================================================== */
 
 class ExpressionParsingMatrix {
-	public:
+public:
+
+	/* ==============================================================================
+		Constructor & Destructor
+	============================================================================== */
+
+	ExpressionParsingMatrix ();
+	~ExpressionParsingMatrix ();
+
+private:
+
+	void createMatrix();
+
+	/* ==============================================================================
+		Public Parsing Functions
+	============================================================================== */
 
 
 
